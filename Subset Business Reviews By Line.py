5c52506e48a7838182026c22a88120b0062527f8
Nick Cohron <dcohron@stevens.edu>
2016-11-14 13:02:04 -0500

    Subset by Line reads the input file line by line.  The other Subset reads the entire file at once.  Unless you have 10GB of memory, it will blow up.
    Subset by line works fast and efficiently, but it is not writing the subset back out to a file the way that I want.  It is writing every element (string) character by character, which is not useful.

--- Subset Business Reviews By Line.py
+++ Subset Business Reviews By Line.py
@@ -12,22 +12,31 @@ import csv
 
 
 def matching_business_ids(path, target_dict):
-    subset = ()
+    subset = []
     
-    with open(path, newline='') as f:
+    with open(path, 'rb') as f:
         reader = csv.reader(f)
         for row in reader:
-            if row[10] in target_dict:
-                subset.extend(row[10], row[13], row[14])
+            if row[4] in target_dict:
+                subset.extend([row[4], row[6], row[2]])
     
     return subset
 
-    
+# procedure to write to file 
+def write_to_file (out_path, data):
+    with open('ur file.csv','wb') as out:
+        csv_out=csv.writer(out)
+        csv_out.writerow(['business_id', 'stars','review'])
+        for row in data:
+            csv_out.writerow(row)
     
 
-# read in the file and put into data frame
+# set variables
 input = '/Users/Nick/Stevens Institute of Technology/Web Analytics/Final Project/data_repo/yelp_academic_dataset_review.csv'
+output = '/Users/Nick/Stevens Institute of Technology/Web Analytics/Final Project/data_repo/restaurant_reviews.csv'
 target_dict = restaurant_dict
 
 match = matching_business_ids(input, target_dict)
-# del data_frame
+
+write_to_file(output, match)
+
