f248d22aabf5d6087a3771dd62f858993169c5fb
Gautam Gite <ggite@stevens.edu>
2016-11-14 23:21:28 -0500

    Made a master dictionary (-stopwords)
    Dictionary from the restaurant business reviews file.

--- Find Attributes.py
+++ Find Attributes.py
@@ -16,8 +16,6 @@ from nltk.tokenize import sent_tokenize
 from nltk import load
 
 
-
-
 def read_file(in_path):
     reviews = []
     with open(in_path, 'rb') as f:
@@ -29,36 +27,42 @@ def read_file(in_path):
     
     
     
-in_path = '/Users/Nick/Stevens Institute of Technology/Web Analytics/Final Project/data_repo/auto_reviews.csv'
+in_path = r'C:\Users\Gautam\Documents\GitHub\Yelp-dataset\csv\rest_biz_review_map.csv'
 
 review_text = read_file(in_path)
 
 stopLex=set(stopwords.words('english'))
 
-
+master_dick={}
 # split the text into sentences 
-sentences=review_text.split('.') 
+# read_file returns a list, can't split a list
+#sentences=review_text.split('.') 
 	
 # for each sentence, set lower case and strip white space 
 # replace non-letter characters with space
 # split sentence into words 
-for sentence in sentences: 
+for review in review_text: 
      # lower case and strip white space
-	sentence=sentence.lower().strip() 
+	review=review.lower().strip() 
     	# replace all non-letter characters with a space
-	sentence=re.sub('[^a-z]',' ',sentence) 
+	review=re.sub(r'[^a-z]',' ',review) 
 	# split on space to get the words in the sentence 
-	words=sentence.split(' ') 
+	words=review.split(' ') 
 
      # for each word in the sentence build frequency word count
 	for word in words:  
          # ignore empty words and stopwords
-         if word=='' or word in stopLex:continue  
-         
+         if word=='' or word in stopLex:
+             continue  
+         elif word in master_dick:
+             master_dick[word]+=1
+         else:
+             master_dick[word]=1
+# open and see master_dick, what people actually write in a review 
  
- 
- 
- 
+sorted_master_dick = sorted(master_dick.items(), key=itemgetter(1), reverse=True)
+
+# next step should be to stem and attach labels
  
  
  
